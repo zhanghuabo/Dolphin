49beaa5c847daf44b4b5cf8dc4199d7cee4d5c8f
zhanghuabo <zhanghuabo_zhb@163.com>
2018-07-02 20:21:28 +0800

    add spring cloud bus to refresh env

--- config-service/src/main/java/com/redteamobile/dolphin/config/ConfigServiceApplication.java
+++ config-service/src/main/java/com/redteamobile/dolphin/config/ConfigServiceApplication.java
@@ -10,7 +10,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableEurekaClient
 public class ConfigServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConfigServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServiceApplication.class, args);
+    }
 }
--- dataplan-service-server/src/main/java/com/redteamobile/dolphin/dataplanserver/controller/HealthController.java
+++ dataplan-service-server/src/main/java/com/redteamobile/dolphin/dataplanserver/controller/HealthController.java
@@ -2,12 +2,14 @@ package com.redteamobile.dolphin.dataplanserver.controller;
 
 import com.redteamobile.dolphin.dataplan.common.response.BaseResponse;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/product/dataPlan/health")
+@RefreshScope
 public class HealthController {
 
     @Value("${env}")
@@ -16,7 +18,7 @@ public class HealthController {
     @Value("${spring.application.name}")
     private String serviceId;
 
-    @GetMapping(value = "/")
+    @GetMapping(value = "")
     public BaseResponse<String> list() {
         String response = "I am " + serviceId + " and env is " + env;
         return BaseResponse.build().success(response);
